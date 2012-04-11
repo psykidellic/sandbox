060d52e26ff42430c676b0f4c34dc129f4bb85f9
RN <riteshn@gmail.com>
2012-04-11 01:37:55 -0700

    Add more html and break it down

--- backbone/main.js
+++ backbone/main.js
@@ -1,9 +1,34 @@
-var AppRounter = Backbone.Router.extend({
+var AppRouter = Backbone.Router.extend({
+
+  routes: {
+    "": "home",
+    "playlist/:name": "playlist"
+  },
+
+  // As the name suggests, the first method to be called for the app.
+  // Set up the page and all the basic views - header, sidebar etc.
+  // Generally, one can check if there
+  // is any existing session or not and change the display if required.
+  initialize: function() {
+    console.log("approuter:initialize");
+    $('header').html('HEADER TEXT ON START');   
+  },
+
+  home: function() {
+    // Generally, homeview do not change so reuse
+    // the same object. Seems like its a good pattern.
+    console.log("home in approuter");
+    if(!this.homeView) {
+      this.homeView = PlaylistCollectionView(new PlaylistCollection(DefaultPlaylist));
+    }
+
+    this.homeView.render();      
+  },
 
 });
 
 $(document).ready(function() {
-  var app = AppRouter();
+  var app = new AppRouter();
   Backbone.history.start();
 });
 
--- backbone/player.js
+++ backbone/player.js
@@ -1,62 +1,114 @@
-(function ($) {
+var Playlist = Backbone.Model.extend({
+  idAttribute: 'name',
 
-  Playlist = Backbone.Model.extend({
-    name: ""
-  });
+  // Default value. So the input box that allows
+  // you to create a new playlist, the default name
+  // is shown.
+  defaults: {
+    'name': 'New playlist'
+  },
 
-  Playlists = Backbone.Collection.extend({
-
-  });
-
-  Song = Backbone.Model.extend({
-    path: ""
-  });
-
-  Songs = Backbone.Model.extend({
-  });
-  
-  Friend = Backbone.Model.extend({
-    //Create a model to hold friend atribute
-    name: null
-  });
-
-  PlayerView = Backbone.View.extend({
-
-
-  });
-  
-  Friends = Backbone.Collection.extend({
-    //This is our Friends collection and holds our Friend models
-    initialize: function (models, options) {
-      this.bind("add", options.view.addFriendLi);
-      //Listen for new additions to the collection and call a view function if so
+  // Ensure that each playlist created has a name
+  initialize: function() {
+    if(!this.get('name')) {
+      console.log('Playlist:initialize');1
+      this.set({'name':this.defaults.name});
     }
-  });
+  },
+
+  url: function () {
+    console.log('create url for playlist: ' + this.get('name'));
+    return '/playlist/' + this.get('name');
+  }   
+});
+
+var PlaylistCollection = Backbone.Collection.extend({
+  model: Playlist,
+  initialize: function (models, options) {
+    console.log("Called pl collection", models, options);
+  }
+});
+
+var PlaylistCollectionView = Backbone.View.extend({
+  el: "#pl-list",
+
+  // Keep reference to initialized view.
+  initialize: function(data) {
+    console.log('PlaylistCollectionView:initialize', data);
+    this.data = data;
+  },
+
+  // Render the collection view. Basically, iterate over
+  // each playlist view and add to the collection.
+  render: function() {
+
+  }
+});
+
+var PlaylistView = Backbone.View.extend({
+  tagName: "li",
+  template: _.tempplate($('#
+
+
+});
+
+//Model.extend({
+    //name: ""
+  //});
+
+  //Playlists = Backbone.Collection.extend({
+
+  //});
+
+  //Song = Backbone.Model.extend({
+    //path: ""
+  //});
+
+  //Songs = Backbone.Model.extend({
+  //});
+  
+  //Friend = Backbone.Model.extend({
+    ////Create a model to hold friend atribute
+    //name: null
+  //});
+
+  //PlayerView = Backbone.View.extend({
+
+
+  //});
+  
+  //Friends = Backbone.Collection.extend({
+    ////This is our Friends collection and holds our Friend models
+    //initialize: function (models, options) {
+      //this.bind("add", options.view.addFriendLi);
+      ////Listen for new additions to the collection and call a view function if so
+    //}
+  //});
 
   
   
-  AppView = Backbone.View.extend({
-    el: $("body"),
-    initialize: function () {
-      this.friends = new Friends( null, { view: this });
-      //Create a friends collection when the view is initialized.
-      //Pass it a reference to this view to create a connection between the two
-    },
-    events: {
-      "click #add-friend":  "showPrompt",
-    },
-    showPrompt: function () {
-      var friend_name = prompt("Who is your friend?");
-      var friend_model = new Friend({ name: friend_name });
-      //Add a new friend model to our friend collection
-      this.friends.add( friend_model );
-    },
-    addFriendLi: function (model) {
-      //The parameter passed is a reference to the model that was added
-      $("#friends-list").append("<li>" + model.get('name') + "</li>");
-      //Use .get to receive attributes of the model
-    }
-  });
+  //AppView = Backbone.View.extend({
+    //el: $("body"),
+    //initialize: function () {
+      //this.friends = new Friends( null, { view: this });
+      ////Create a friends collection when the view is initialized.
+      ////Pass it a reference to this view to create a connection between the two
+    //},
+    //events: {
+      //"click #add-friend":  "showPrompt",
+    //},
+    //showPrompt: function () {
+      //var friend_name = prompt("Who is your friend?");
+      //var friend_model = new Friend({ name: friend_name });
+      ////Add a new friend model to our friend collection
+      //this.friends.add( friend_model );
+    //},
+    //addFriendLi: function (model) {
+      ////The parameter passed is a reference to the model that was added
+      //$("#friends-list").append("<li>" + model.get('name') + "</li>");
+      ////Use .get to receive attributes of the model
+    //}
+  //});
   
-  var appview = new AppView;
-})(jQuery);
+  //var appview = new AppView;
+//})(jQuery);
