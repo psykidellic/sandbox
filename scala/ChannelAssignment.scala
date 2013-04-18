a9022060765cc2df3682d56ee3ab1806f98cc452
RN <riteshn@gmail.com>
2013-04-17 23:25:09 -0700

    add scalal hello world to try out stuff

---
+++ scala/ChannelAssignment.scala
@@ -0,0 +1,14 @@
+import collection.mutable.HashMap
+
+object XXX {
+  def ritesh() {
+    println("ritesh")
+  }
+}
+
+object ChannelAssignment {
+
+  def assignChannel(input: HashMap[Long, Any]): Int = {
+    1
+  }
+}
