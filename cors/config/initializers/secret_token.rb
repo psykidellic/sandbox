06969966eacb50072513c960c7d6992f77da93a9
psykidellic <riteshn@gmail.com>
2013-07-28 11:55:20 -0700

    "Cors example" - unfortunately this works.

---
+++ cors/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cors::Application.config.secret_token = '3f6b82a6db86a5eddabbc54ef501f71e3abbd6d0d505de49fe635f8cb17bfe1d41e53cf6d9f11e1acfeb5b7f76c1417cc24625c9040ba946015f01edfc9abd83'
