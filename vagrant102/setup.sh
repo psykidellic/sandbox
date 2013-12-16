64fa5ff069b7466c9149016b57b875e06e2dc776
psykidellic <riteshn@gmail.com>
2013-12-16 08:56:06 -0800

    Setup that correctly creates symlink of database.yml.development into database.yml.

--- vagrant102/setup.sh
+++ vagrant102/setup.sh
@@ -1,21 +1,25 @@
-apt-get update
-apt-get dist-upgrade
-apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
+# apt-get update
+# apt-get dist-upgrade
+# apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
 
-apt-get install -y nginx
+# apt-get install -y nginx
 
-# Install the latest postgresql
-# Unfortunately seems they dont have for saucy yet
-# echo "deb http://apt.postgresql.org/pub/repos/apt/ raring-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-# sudo apt-get update
-# sudo apt-get install postgresql-9.3
+# # Install the latest postgresql
+# # Unfortunately seems they dont have for saucy yet
+# # echo "deb http://apt.postgresql.org/pub/repos/apt/ raring-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+# # wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# # sudo apt-get update
+# # sudo apt-get install postgresql-9.3
 
-apt-get install -y postgresql postgresql-server-dev-9.1
+# apt-get install -y postgresql postgresql-server-dev-9.1
 
-apt-get install -y ruby-dev
-gem install rails --version 3.2.13 --no-ri --no-rdoc
-gem install pg
+# apt-get install -y ruby-dev
+# gem install rails --version 3.2.13 --no-ri --no-rdoc
+# gem install pg
 
-su - postgres -c "psql -U postgres -d postgres -c \"create role vagrant with createdb login password 'password1'\""
-su - vagrant -c "createdb beamtous_development"
+# su - postgres -c "psql -U postgres -d postgres -c \"create role vagrant with createdb login password 'password1'\""
+# su - vagrant -c "createdb beamtous_development"
+
+ln -nfs /vagrant/config/database.yml.development /vagrant/config/database.yml
+
+
