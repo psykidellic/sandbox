1e5c9903ba9396156162b76e47cd1374acf443cb
psykidellic <riteshn@gmail.com>
2013-12-16 12:32:34 -0800

    Now that we dont have sqlite on development also, just remove the reference in Gemfile.
    Also, turns out that just updating the Gemfile and running install
    failed - it never really installed pg. Not sure why.
    
    Also, add rubyracer for js runtime.

--- vagrant102/setup.sh
+++ vagrant102/setup.sh
@@ -1,26 +1,35 @@
-# apt-get update
-# apt-get dist-upgrade
-# apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
+apt-get update
+apt-get dist-upgrade
+apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
 
-# apt-get install -y nginx
+apt-get install -y nginx
 
-# # Install the latest postgresql
-# # Unfortunately seems they dont have for saucy yet
-# # echo "deb http://apt.postgresql.org/pub/repos/apt/ raring-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-# # wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-# # sudo apt-get update
-# # sudo apt-get install postgresql-9.3
+# Install the latest postgresql
+# Unfortunately seems they dont have for saucy yet
+# echo "deb http://apt.postgresql.org/pub/repos/apt/ raring-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get install postgresql-9.3
 
-# apt-get install -y postgresql postgresql-server-dev-9.1
+apt-get install -y postgresql postgresql-server-dev-9.1
 
-# apt-get install -y ruby-dev
-# gem install rails --version 3.2.13 --no-ri --no-rdoc
-# gem install pg
+apt-get install -y ruby-dev
+gem install rails --version 3.2.13 --no-ri --no-rdoc
+gem install pg
 
-# su - postgres -c "psql -U postgres -d postgres -c \"create role vagrant with createdb login password 'password1'\""
-# su - vagrant -c "createdb beamtous_development"
+su - postgres -c "psql -U postgres -d postgres -c \"create role vagrant with createdb login password 'password1'\""
+su - vagrant -c "createdb beamtous_development"
 
-# ln -nfs /vagrant/config/database.yml.development /vagrant/config/database.yml
+ln -nfs /vagrant/config/database.yml.development /vagrant/config/database.yml
 
-rm /etc/nginx/sites-enabled/default
+cd /vagrant
+bundle install --binstubs
+
+rm /etc/nginx/sites-enabled/default 2>/dev/null
 ln -nfs /vagrant/config/nginx.conf.development /etc/nginx/sites-enabled/beamtous
+service nginx restart
+
+bundle exec unicorn -c config/unicorn_development.rb -D
+chmod +x config/unicorn_development_init.sh
+ln -nfs /vagrant/config/unicorn_development_init.sh /etc/init.d/unicorn
+service unicorn restart
