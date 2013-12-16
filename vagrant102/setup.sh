02c44365c7d89b5805cede054452be76f5383284
psykidellic <riteshn@gmail.com>
2013-12-15 23:17:14 -0800

    Start vagrant102. This time, i will have a simple rails app that i can work upon.

---
+++ vagrant102/setup.sh
@@ -0,0 +1,18 @@
+# apt-get update
+# apt-get dist-upgrade
+# apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
+
+# apt-get install -y nginx
+
+# Install the latest postgresql
+# Unfortunately seems they dont have for saucy yet
+# echo "deb http://apt.postgresql.org/pub/repos/apt/ raring-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get install postgresql-9.3
+
+# apt-get install -y postgresql postgresql-server-dev-9.1
+
+# apt-get install -y ruby-dev
+# gem install rails --version 3.2.13 --no-ri --no-rdoc
+# gem install pg
