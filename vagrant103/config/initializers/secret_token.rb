274a081bf85b3ca37270852bb46c6ed9bd7fa7ad
psykidellic <riteshn@gmail.com>
2013-12-19 13:34:58 -0800

    Add a rails app so we can test the vagrant setup.

---
+++ vagrant103/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vagrant103::Application.config.secret_token = 'b4fef7edccfb1771c2bbf53530303ab7b95594b3a6d7829f014cf0993a83fa2a9e589ff038a8f1d1183a5212b830ca4d61b5afbea60b47cea278edbcb412a945'
