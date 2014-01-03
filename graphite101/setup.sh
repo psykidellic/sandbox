3969c1a2c72a7306cddaa31e310a235e57ab631c
psykidellic <riteshn@gmail.com>
2014-01-02 23:10:56 -0800

    Some work to get nginx with graphite going.

--- graphite101/setup.sh
+++ graphite101/setup.sh
@@ -3,32 +3,32 @@ apt-get dist-upgrade
 
 apt-get install -y python-pip
 
+# Ideas taken from: https://gist.github.com/obazoud/6711794
+
+apt-get install --assume-yes  build-essential python-dev  memcached python-cairo-dev python-django python-ldap python-memcache python-pysqlite2 sqlite3 erlang-os-mon erlang-snmp rabbitmq-server expect ssh python-setuptools nginx
+
 pip install whisper
 pip install carbon
 pip install graphite-web
+pip install uwsgi
 
 cp /opt/graphite/conf/carbon.conf.example /opt/graphite/conf/carbon.conf
 cp /opt/graphite/conf/storage-schemas.conf.example /opt/graphite/conf/storage-schemas.conf
-cp /opt/graphite/conf/graphite.wsgi.example /opt/graphite/conf/graphite.wsgi
-
-# Ideas taken from: https://gist.github.com/obazoud/6711794
-
-apt-get install --assume-yes apache2 apache2-mpm-worker apache2-utils apache2.2-bin apache2.2-common libapr1 libaprutil1 libaprutil1-dbd-sqlite3 build-essential python-dev libapache2-mod-wsgi libaprutil1-ldap memcached python-cairo-dev python-django python-ldap python-memcache python-pysqlite2 sqlite3 erlang-os-mon erlang-snmp rabbitmq-server bzr expect ssh libapache2-mod-python python-setuptools
+cp /opt/graphite/conf/graphite.wsgi.example /opt/graphite/conf/wsgi.py
 
 easy_install django-tagging
 
-cp /opt/graphite/examples/example-graphite-vhost.conf /etc/apache2/sites-available/default
+rm /etc/nginx/sites-enabled/default
+cp /vagrant/graphite.nginx /etc/nginx/sites-enabled/graphite.conf
 
-mkdir /etc/httpd
-mkdir /etc/httpd/wsgi
-
-/etc/init.d/apache2 reload
+service nginx restart
 
 cd /opt/graphite/webapp/graphite/
 python manage.py syncdb
 
 chown -R www-data:www-data /opt/graphite/storage/
-/etc/init.d/apache2 restart
+
+service nginx restart
 
 cd /opt/graphite/webapp/graphite
 cp local_settings.py.example local_settings.py
